1f7ae0776fe881ed5856a3ba9cb6dbcf22fa08c0
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-09 22:05:18 +0100

    Refactor app.js to use individual functions to build up a new cat

--- public/app.js
+++ public/app.js
@@ -1,24 +1,40 @@
-const app = function() {
+var createCatUl = function(){
   // 1. create the cat <ul> and its class
   let catUl = document.createElement('ul');
   catUl.classList.add('cat');
+  return catUl;
+};
 
+var createNameLi = function(catName){
   // 2. create the first child - <li> name
   let nameLi = document.createElement('li');
-  nameLi.innerText  = 'Name: Lebowski';
+  nameLi.innerText = 'Name: ' + catName;
+  return nameLi;
+};
 
+var createFavFoodLi = function(favFood){
   // 3. create the second child - <li> favourite food
   let favFoodLi = document.createElement('li');
-  favFoodLi.innerText = 'Favourite food: Dreamies';
+  favFoodLi.innerText = 'Favourite food: ' + favFood;
+  return favFoodLi;
+};
 
+var createImageLi = function(){
   // 4. create the third child <li>
   let imageLi = document.createElement('li');
+  return imageLi;
+};
 
+var createImage = function(imageSrc){
   // 5. create the child <img> inside the third <li>
   let catImage = document.createElement('img');
   catImage.width = '500';
-  catImage.src = 'https://78.media.tumblr.com/2675ae097a77bf9f0136ac09cbcd7712/tumblr_p67ym7RQYX1whb48xo1_500.jpg';
+  catImage.src = imageSrc;
+  //catImage.src = 'https://78.media.tumblr.com/2675ae097a77bf9f0136ac09cbcd7712/tumblr_p67ym7RQYX1whb48xo1_500.jpg';
+  return catImage;
+};
 
+var appendElements = function(catUl, nameLi, favFoodLi, imageLi, catImage){
   // 9. append the name <li> to the <ul>
   catUl.appendChild(nameLi);
 
@@ -33,9 +49,17 @@ const app = function() {
 
   let cats = document.querySelector("#cats");
   cats.appendChild(catUl);
-
-  // debugger;
-
 };
 
-window.onload = app;
+var addCat = function(catName, favFood, catImage){
+  var catUl = createCatUl();
+  var nameLi = createNameLi(catName);
+  var favFoodLi = createFavFoodLi(favFood);
+  var imageLi = createImageLi();
+  var catImage = createImage(catImage);
+  appendElements(catUl, nameLi, favFoodLi, imageLi, catImage);
+};
+
+window.onload = function(){
+  addCat('Lebowski', 'Dreamies', 'https://78.media.tumblr.com/2675ae097a77bf9f0136ac09cbcd7712/tumblr_p67ym7RQYX1whb48xo1_500.jpg');
+};
