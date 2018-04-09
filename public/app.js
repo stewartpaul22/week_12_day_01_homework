098874e607c7766f1a2b2c265de1c132789466c4
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-09 21:33:01 +0100

    Add cats.appendChild(catUL) to display new cat in browser

--- public/app.js
+++ public/app.js
@@ -1,22 +1,21 @@
-var app = function() {
-
+const app = function() {
   // 1. create the cat <ul> and its class
-  const catUl = document.createElement('ul');
+  let catUl = document.createElement('ul');
   catUl.classList.add('cat');
 
   // 2. create the first child - <li> name
-  const nameLi = document.createElement('li');
+  let nameLi = document.createElement('li');
   nameLi.innerText  = 'Name: Lebowski';
 
   // 3. create the second child - <li> favourite food
-  const favFoodLi = document.createElement('li');
+  let favFoodLi = document.createElement('li');
   favFoodLi.innerText = 'Favourite food: Dreamies';
 
   // 4. create the third child <li>
-  const imageLi = document.createElement('li');
+  let imageLi = document.createElement('li');
 
   // 5. create the child <img> inside the third <li>
-  const catImage = document.createElement('img');
+  let catImage = document.createElement('img');
   catImage.width = '500';
   catImage.src = 'https://78.media.tumblr.com/2675ae097a77bf9f0136ac09cbcd7712/tumblr_p67ym7RQYX1whb48xo1_500.jpg';
 
@@ -32,9 +31,11 @@ var app = function() {
   // 7. append the image <li> to the <ul>
   catUl.appendChild(imageLi);
 
+  let cats = document.querySelector("#cats");
+  cats.appendChild(catUl);
 
+  // debugger;
 
-  debugger;
 };
 
 window.onload = app;
